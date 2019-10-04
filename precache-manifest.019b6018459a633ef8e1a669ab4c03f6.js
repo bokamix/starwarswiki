63204c5659951b1996562845e72fef76ae129447
bokamix <janiakpol@gmail.com>
2019-10-04 22:35:25 +0200

    Updates

---
+++ precache-manifest.019b6018459a633ef8e1a669ab4c03f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0314a81b445c214a00101f25d47aed99",
+    "url": "/starwarswiki/index.html"
+  },
+  {
+    "revision": "0d301f69861ddb4361b2",
+    "url": "/starwarswiki/static/css/main.f42ee710.chunk.css"
+  },
+  {
+    "revision": "58f3869fbbef72d58464",
+    "url": "/starwarswiki/static/js/2.ee8f1aad.chunk.js"
+  },
+  {
+    "revision": "0d301f69861ddb4361b2",
+    "url": "/starwarswiki/static/js/main.51e67e0e.chunk.js"
+  },
+  {
+    "revision": "445e1a99a4231f9f2fe0",
+    "url": "/starwarswiki/static/js/runtime-main.a00fb882.js"
+  },
+  {
+    "revision": "81e948ad0179ac79be6c28982906dae1",
+    "url": "/starwarswiki/static/media/ARROWCLOSE.81e948ad.svg"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/starwarswiki/static/media/ARROWOPEN.da45c4c0.svg"
+  },
+  {
+    "revision": "4fdbb74fd733a63657da172fdebf0feb",
+    "url": "/starwarswiki/static/media/DELETE.4fdbb74f.svg"
+  },
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/starwarswiki/static/media/LOGO.79977f05.svg"
+  },
+  {
+    "revision": "d8b5abddb11a20073189e3c1e56bd14f",
+    "url": "/starwarswiki/static/media/SEARCH.d8b5abdd.svg"
+  },
+  {
+    "revision": "90bdefe4beb92394073f0487415977a0",
+    "url": "/starwarswiki/static/media/SortArrows.90bdefe4.svg"
+  }
+]);
